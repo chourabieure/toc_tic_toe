7d302dc02181841c92ee60f6576b86841a6e6995
chourabieure <matsim@orange.fr>
2020-11-25 16:17:01 +0100

    first

---
+++ js/main.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+
+    board = new Board();
+    var currPlayer = 'X';
+    $('.cell').css('cursor','pointer');
+
+    $('.cell').click( function(){
+        let clickedCell = $(this).attr('id');
+        //if the cell is empty, we add the current user
+        if($('#'+clickedCell).is(':empty')){
+            $('#'+clickedCell).append('<h1>'+currPlayer+'</h1>');
+            $('#'+clickedCell).css('cursor','not-allowed')
+            board.setState(clickedCell,currPlayer);
+        }
+        if(currPlayer == 'X'){
+            currPlayer = 'O';
+        }else{
+            currPlayer = 'X';
+        }
+        if(winner = board.hasWinner()){
+            if(winner == 'none'){
+                $('#whoWin').append("It's a draw !");
+            }else{
+                $('#whoWin').append(winner+' a gagné !');
+            }
+            $('.win-message').first().css('display','flex');
+        }
+    });
+
+    $('#restart').click( function(){
+        board.reset();
+        $('#whoWin').empty();
+        $('.win-message').first().css('display','none');
+        $('.cell').empty();
+        $('.cell').css('cursor','pointer');
+    });
+
+
+
+});
+
+class Board{
+
+    state = [0,0,0,
+             0,0,0,
+             0,0,0]
+
+    setState(i,value){
+        this.state[i-1]=value;
+    }
+    isFull(){
+        for(let i = 0; i < 9 ; i++){
+            if(this.state[i] == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    reset(){
+       this.state = [0,0,0,
+                     0,0,0,
+                     0,0,0]; 
+    }
+
+    hasWinner(){
+        if(this.isFull()){
+            return 'none';
+        }
+        // check column and lines
+        for(let i = 0; i < 3; i++){
+            // check column
+            if(this.state[0+i] == 'X' && this.state[3+i] == 'X' && this.state[6+i] == 'X'){
+                return 'X';
+            }
+            if(this.state[0+i] == 'O' && this.state[3+i] == 'O' && this.state[6+i] == 'O'){
+                return 'O';
+            }
+            //check lines
+            if(this.state[0+(i*3)] == 'X' && this.state[1+(i*3)] == 'X' && this.state[2+(i*3)] == 'X'){
+                return 'X';
+            }
+            if(this.state[0+(i*3)] == 'O' && this.state[1+(i*3)] == 'O' && this.state[2+(i*3)] == 'O'){
+                return 'O';
+            }
+        }
+        //check top left diag
+        if(this.state[0] == 'X' && this.state[4] == 'X' && this.state[8] == 'X'){
+            return 'X';
+        }
+        if(this.state[0] == 'O' && this.state[4] == 'O' && this.state[8] == 'O'){
+            return 'O';
+        }
+        // check top right diag
+        if(this.state[2] == 'X' && this.state[4] == 'X' && this.state[6] == 'X'){
+            return 'X';
+        }
+        if(this.state[2] == 'O' && this.state[4] == 'O' && this.state[6] == 'O'){
+            return 'O';
+        }
+        return false;
+    }
+
+
+}
